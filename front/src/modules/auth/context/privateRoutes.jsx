e10973cb672bab6b36587b9bb5912fd359e3a650
Julio Cesar Gutierrez Rojas <20203tn160@utez.edu.mx>
2025-02-24 13:17:51 -0600

    FIX: Validation role

--- front/src/modules/auth/context/privateRoutes.jsx
+++ front/src/modules/auth/context/privateRoutes.jsx
@@ -1,43 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 const PrivateRoutes = ({ role, children }) => {
   const { user } = useContext(AuthContext);
-  const [alertMessage, setAlertMessage] = useState("");
 
-  useEffect(() => {
-    // Si hay un mensaje de alerta, redirigir después de 3 segundos
-    if (alertMessage) {
-      setTimeout(() => {
-        setAlertMessage(""); // Limpiar el mensaje de alerta
-        // Aquí podrías hacer una redirección condicional si fuera necesario
-      }, 3000); // 3 segundos
-    }
-  }, [alertMessage]);
-
-  if (!user) {
-    setAlertMessage("No estás registrado o no tienes sesión activa.");
-    return <Navigate to="/" />;
-  }
-
-  if (role === "admin" && user.role !== "admin") {
-    setAlertMessage("No tienes permisos de administrador");
-    return <Navigate to="/userPanel" />;
-  }
-
-  if (role === "usuario" && user.role !== "usuario") {
-    setAlertMessage("No tienes permisos de usuario");
-    return <Navigate to="/" />;
-  }
-
-  if (!user || user.role !== role) {
-    window.location.href = "/403";  
+  if (!user || !user.role || user.role !== "admin") {
+    return <Navigate to="/403" />;
   }
 
   return children || <Outlet />;
 };
 
-
 export default PrivateRoutes;
-
